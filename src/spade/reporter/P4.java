7798ed962f995401d4c28d061ccf1711ec7bf1fb
Dhiraj Saharia <dsaharia@outlook.com>
2024-09-30 19:47:58 -0400

    added rabbitmq client integration and message splitting

--- src/spade/reporter/P4.java
+++ src/spade/reporter/P4.java
@@ -19,24 +19,22 @@
  */
 package spade.reporter;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.LinkedHashMap;
+import java.io.IOException;
+// import java.util.LinkedHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import spade.core.AbstractEdge;
+// import spade.core.AbstractEdge;
 import spade.core.AbstractReporter;
-import spade.core.AbstractVertex;
-import spade.core.Edge;
-import spade.core.Vertex;
-import spade.utility.Execute;
+// import spade.core.AbstractVertex;
+// import spade.core.Edge;
+// import spade.core.Vertex;
+// import spade.utility.Execute;
 
-// import com.rabbitmq.client.Channel;
-// import com.rabbitmq.client.Connection;
-// import com.rabbitmq.client.ConnectionFactory;
-// import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 /**
  * Prov reporter for P4.
@@ -44,20 +42,70 @@ import spade.utility.Execute;
  * @author Dhiraj Saharia
  */
 public class P4 extends AbstractReporter {
-
+    // Configure the RabbitMQ parameters once
     private volatile boolean shutdown;
-    //private HashMap<String, AbstractVertex> vertices;
     private Logger logger = Logger.getLogger(P4.class.getName());
+    ConnectionFactory factory = null;
+    Connection connection = null;
+    Channel channel = null;
+
+    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        String message = new String(delivery.getBody(), "UTF-8");
+        parseMessage(message);
+        logger.log(Level.INFO, " [x] Received '" + message + "'");
+    };
+
+    private void initializeRabbitMQ(String QUEUE_NAME) {
+        factory = new ConnectionFactory();
+        // TODO - This parameters should come from a config file
+        factory.setHost("172.18.0.2");
+        factory.setPort(5672);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        try {
+            connection = factory.newConnection();
+            try {
+                channel = connection.createChannel();
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                logger.log(Level.INFO, "RabbitMQ queue declared successfully");
+            }
+            catch (Exception error){
+                logger.log(Level.WARNING, "Failed to initialize connection",error.toString());
+            }
+        }
+        catch (Exception error){
+            logger.log(Level.WARNING, "Failed to initialize connection", error);
+        }
+    }
+    private void parseMessage(String data) {
+        String[] parts = data.split("\\|");
+    }
+
 
     @Override
     public boolean launch(String arguments) {
-        logger.log(Level.INFO, "Hello P4");
+        logger.log(Level.INFO, "P4 reporter started");
+        String QUEUE_NAME = "test";
+        initializeRabbitMQ(QUEUE_NAME);
+
+        while (!shutdown) {
+            try {
+                channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+            }
+            catch (Exception error) {
+                logger.log(Level.WARNING, "Failed to consume message from queue", error);
+            }
+        }
         return true;
 	}
 
     @Override
     public boolean shutdown() {
-        logger.log(Level.INFO, "BYE");
+        shutdown = true;
+        // TODO - close connection function
+        logger.log(Level.INFO, "Closing channel and connection");
+        // channel.close();
+        // connection.close();
         return true;
     }
 
